40a55eb02ee3e73ef15240d00b961ebb36a8ffc7
Raquel <raquel.oliamaral@gmail.com>
2025-08-05 09:54:57 -0300

    Exercício fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n-2);
+}
+
+int main(){
+    int i;
+    int numero;
+    printf("Sequência de Fibonacci\n");
+    printf("Digite um número: ");
+    scanf("%i", &numero);
+    for (i = 0; i < numero; i++){
+        printf("%d \n", fibonacci(i));
+    }
+    return 0;
+}
