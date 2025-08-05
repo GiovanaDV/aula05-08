854c47bb8a209324ab8ca027e3373cbeb1d9fb5e
Giovana <giovana.dvalentini@gmail.com>
2025-08-05 09:38:00 -0300

    Commit dos códigos fatorial.c e potencia.c

---
+++ fatorial.c
@@ -0,0 +1,17 @@
+// 5! = 5 * 4 * 3 * 2 * 1
+#include <stdio.h>
+int fatorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1; // caso base
+    } else {
+        return n * fatorial(n - 1); // chamada recursiva
+    }
+}
+int main() {
+    int numero;
+    printf("Digite um numero: ");
+    scanf("%i", &numero);
+    printf("Fatorial de %d = %d\n", numero, fatorial(numero));
+    return 0;
+}
+
