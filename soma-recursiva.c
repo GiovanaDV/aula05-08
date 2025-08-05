91bade38bc03153849095bea11431de469287ed8
Eduardoui <eduardobacelar2007@gmail.com>
2025-08-05 09:50:02 -0300

    commit

---
+++ soma-recursiva.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int soma(int n) {
+    if (n == 0){
+        return 0;
+    } else {
+        return n + soma(n - 1);
+    } 
+}
+
+int main (){
+    int n;
+    printf("Digite um número: ");
+    scanf("%i", &n);
+    printf("A soma dos primeiros %d números é: %d\n", n, soma(n));
+    return 0;
+}
